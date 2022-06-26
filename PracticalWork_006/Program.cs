23c5ca966590ada521c39dd205a03f99c13e0f5c
igor <igoshinig@yandex.ru>
2022-06-26 12:45:37 +0500

    Первая задача решина

--- PracticalWork_006/Program.cs
+++ PracticalWork_006/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+int number1;
+int number2;
+
+Console.Write("Введите число: ");
+number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+number2 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1, min = number2;
+
+if (number2 > max) max = number2;
+if (number1 < max) min = number1;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
+
+
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
